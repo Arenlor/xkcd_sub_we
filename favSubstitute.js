efaed67ef1020b0e76b56a5031a8babfd78b08db
Arenlor <arenlor@arenlor.com>
2017-03-05 16:58:14 -0500

    Working prototype
    This works, though not as well as I'd care for. Seems to screw up sites. Second and third comic need added for news, along with options.
    
    Signed-off-by: Arenlor <arenlor@arenlor.com>

---
+++ favSubstitute.js
@@ -0,0 +1,8 @@
+/* A version for both upper and lower cases for each */
+document.body.innerHTML = document.body.innerHTML.replace(/Keyboard/g, "Leopard");
+document.body.innerHTML = document.body.innerHTML.replace(/keyboard/g, "leopard");
+document.body.innerHTML = document.body.innerHTML.replace(/Force/g, "Horse");
+document.body.innerHTML = document.body.innerHTML.replace(/force/g, "horse");
+document.body.innerHTML = document.body.innerHTML.replace(/My cat/g, "My friend Catherine");
+document.body.innerHTML = document.body.innerHTML.replace(/my cat/g, "my friend Catherine");
+document.body.innerHTML = document.body.innerHTML.replace(/My Cat/g, "My friend Cahterine");
--- newsSubstitute.js
+++ newsSubstitute.js
@@ -1,1 +1,64 @@
-document.body.textContent.replace(/Witnesses/g, "These dudes I know");
+/* A version for both upper and lower cases for each */
+document.body.innerHTML = document.body.innerHTML.replace(/Witnesses/g, "These dudes I know");
+document.body.innerHTML = document.body.innerHTML.replace(/witnesses/g, "these dudes I know");
+document.body.innerHTML = document.body.innerHTML.replace(/Allegedly/g, "Kinda probably");
+document.body.innerHTML = document.body.innerHTML.replace(/allegedly/g, "kinda probably");
+document.body.innerHTML = document.body.innerHTML.replace(/New study/gi, "Tumblr post");
+document.body.innerHTML = document.body.innerHTML.replace(/Rebuild/g, "Avenge");
+document.body.innerHTML = document.body.innerHTML.replace(/rebuild/g, "avenge");
+document.body.innerHTML = document.body.innerHTML.replace(/Space/g, "Spaaace");
+document.body.innerHTML = document.body.innerHTML.replace(/space/g, "spaaace");
+document.body.innerHTML = document.body.innerHTML.replace(/Google Glass/gi, "Virtual Boy");
+document.body.innerHTML = document.body.innerHTML.replace(/Smartphone/gi, "Pokédex");
+document.body.innerHTML = document.body.innerHTML.replace(/Smart phone/gi, "Pokédex");
+document.body.innerHTML = document.body.innerHTML.replace(/Electric/g, "Atomic");
+document.body.innerHTML = document.body.innerHTML.replace(/electric/g, "atomic");
+document.body.innerHTML = document.body.innerHTML.replace(/Senator/gi, "Elf-Lord");
+document.body.innerHTML = document.body.innerHTML.replace(/Car/g, "Cat");
+document.body.innerHTML = document.body.innerHTML.replace(/car/g, "cat");
+document.body.innerHTML = document.body.innerHTML.replace(/Election/g, "Eating contest");
+document.body.innerHTML = document.body.innerHTML.replace(/election/g, "eating contest");
+document.body.innerHTML = document.body.innerHTML.replace(/Congressional Leaders/gi, "River Spirits");
+document.body.innerHTML = document.body.innerHTML.replace(/Homeland Security/gi, "Homestar Runner");
+document.body.innerHTML = document.body.innerHTML.replace(/Could not be reached for comment/g, "Is guilty and everyone knows it");
+document.body.innerHTML = document.body.innerHTML.replace(/could not be reached for comment/g, "is guilty and everyone knows it");
+/* é = 0233; à = 0224
+debate - dance off
+self driving - uncontrollably swerving
+poll - psychic reading
+candidate - airbender
+drone - dog
+vows to - probably won't
+at large - very large
+successfully - suddenly
+expands - physically expands
+first/second/third-degree - friggin' awful
+an unknown number - like hundreds
+front runner - blade runner
+global - spherical
+years - minutes
+minutes - years
+no indication - lots of signs
+urged restraint by - drunkenly egged on
+horsepower - tons of horsemeat */
+/*
+gaffe - magic spell
+ancient - haunted
+star-studded - blood-soaked
+remains to be seen - will never be known
+silver bullet - way to kill werewolves
+subway system - tunnels i found
+surprising - surprising (but not to me)
+war of words - interplanetary war
+tension - sexual tension
+cautiously optimistic - delusional
+doctor who - the big bang theory
+win votes - find pokémon
+behind the headlines - beyond the grace
+email/facebook post/tweet - poem
+facebook ceo - this guy
+latest - final
+disrupt - destroy
+meeting - Ménage à trois
+scientists - channing tatum and his friends
+you won't believe - i'm really sad about */
